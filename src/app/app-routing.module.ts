30e60b80fa156bd88dfa24c7916ed46b63941ff1
Cristian Manzano <cristianmanzano1989@gmail.com>
2018-12-10 11:56:19 -0500

    User lis Created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
+import { UserComponent } from './user/user.component';
 import { AppComponent } from './app.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 
 const routes: Routes = [
-    {path:'',redirectTo:'/appComponent',pathMatch:'full'},
-    {path:'appComponent',component:AppComponent}
+    {path:'',redirectTo:'/userComponent',pathMatch:'full'},
+    {path:'appComponent',component:AppComponent},
+    {path:'userComponent',component:UserComponent}
 ];
 
 export const appRouting = RouterModule.forRoot(routes);
